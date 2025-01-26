5706d3ebdeb5e46f73c00dbadbe9fd1d194022bc
conspicio-ok <raphael.vingt.cinq@ikmail.com>
2025-01-26 01:24:05 +0100

    Hyprpaper : Error with script, patch

--- dotfiles/scripts/rdm_img.sh
+++ dotfiles/scripts/rdm_img.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-NUMBER=$(ls -l $HOME/Background | wc -l)
+MAX=$(ls -l $HOME/Background/* | wc -l)
+NUMBER=$(shuf -i 1-$MAX -n 1)
 ((NUMBER--))
 WALL="wall${NUMBER}.png"
 sleep 2
-hyprctl hyprpaper preload="$HOME/Background/${WALL}"
-hyprctl hyprpaper wallpaper="$HOME/Background/${WALL}"
+hyprctl hyprpaper preload "$HOME/Background/${WALL}"
+hyprctl hyprpaper wallpaper ",$HOME/Background/${WALL}"
