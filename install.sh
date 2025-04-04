1dcc4f819016df4c356f630f8bb953ce384dd336
conspicio <raphael.vingt.cinq@ikmail.com>
2025-04-05 00:04:17 +0200

    corrige path

--- install.sh
+++ install.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-source $HOME/.config/dotfiles/scripts/print.sh
+function print_section {
+	col=$((80));
+	spaces=$((0));
+	col=$(($col-$spaces*4-${#1}));
+	string='';
 
+	for ((i=0; i<$spaces; i++)) {
+		string=$string' ';
+	}
+	for ((i=0; i<$(($col/2)); i++)) {
+		string=$string'-';
+	}
+	string=$string$1$string
+
+	echo $string;
+}
 
 cd;
 
