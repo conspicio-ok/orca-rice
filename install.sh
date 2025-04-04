7cf19d08a1aa04d2b18ea10b43c3e06427ef6cfb
conspicio <raphael.vingt.cinq@ikmail.com>
2025-04-04 23:52:22 +0200

    set the install script

--- install.sh
+++ install.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
-mkdir ~/.config 2> tmp.txt
-mv .fonts ~/.fonts 2> tmp.txt
-mv ./* ~/.config 2> tmp.txt
-mv ./.* ~/.config 2> tmp.txt
+source $HOME/.config/dotfiles/scripts/print.sh
+
+
+cd;
+
+orca='orca-rice';
+if [ -d $orca ];
+then
+	echo "$orca is not found, please move it at your home !";
+else
+	print_section  "CREATE '.config' FOLDER";
+	mkdir $HOME/.config 2> tmp.txt
+	echo ;
+	echo ;
+	
+	print_section "MOVE CURRENTS FILES IN THE .CONFIG FOLDER"
+	mv $HOME/orca-rice/* $HOME/.config 2> tmp.txt
+	mv $HOME/orca-rice/.* $HOME/.config 2> tmp.txt
+	echo ;
+	echo ;
+	
+	print_section "PACKAGES INSTALL";
+	bash $HOME/.config/dotfiles/scripts/install_packages.sh;
+	echo ;
+	echo ;
+fi
+
 
